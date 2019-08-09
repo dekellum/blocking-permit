e1ad9d984cbf980a960b8c23acc6663f5dc33f87
David Kellum <dek-oss@gravitext.com>
2019-08-09 14:36:07 -0700

    add BlockingPermitFuture and await style usage and testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,199 @@
 // https://github.com/tokio-rs/tokio/issues/588
 // https://github.com/tokio-rs/tokio/issues/1177
 
+#![warn(rust_2018_idioms)]
+#![feature(async_await)]
+
+use std::fmt;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::task::{Context, Poll};
 use std::thread;
 
-use futures::channel::oneshot;
-
-use tokio_sync::semaphore::{Permit, Semaphore};
-
-/*
-pub fn blocking<F, T>(f: F) -> Poll<Result<T, BlockingError>>
-where
-    F: FnOnce() -> T,
-{
-}
-
-Returns:
-
-Pending
-  - CT: queue at capacity
-  - MT: blocking capacity (run again)
-
-Ready:
-  - CT: queued (no result) <- How to garuntee ordering?
-  - MT: run in blocking mode (with result)
-
-*/
+use tokio_sync::{
+    oneshot,
+    semaphore::{Permit, Semaphore},
+};
 
 /// A scoped permit for blocking operations. When dropped (out of scope or
 /// manually), the permit is released.
+#[must_use]
 pub struct BlockingPermit<'a> {
     permit: Option<(Permit, &'a Semaphore)>,
+    entered: AtomicBool
 }
 
-impl<'a> Drop for BlockingPermit<'a> {
-    fn drop(&mut self) {
-        eprintln!("Dropping BlockingPermit!");
+/// A future which resolves to a [`BlockingPermit`], created via the
+/// [`blocking_permit_future`] function.
+#[must_use]
+pub struct BlockingPermitFuture<'a> {
+    semaphore: &'a Semaphore,
+    acquired: AtomicBool,
+}
 
-        // TODO: exit_blocking_section();
+// TODO: Complete application of must_use attributes above or elsewhere
 
-        if let Some((ref mut permit, ref semaphore)) = self.permit {
-            permit.release(semaphore);
+// TODO: Decide if the use of AtomicBool in the above (vs just bool) is really
+// warranted. If it is, consider relaxing from SeqCst.
+
+// TODO: Remove or replace all eprintln calls with `log` below.
+
+impl<'a> Future for BlockingPermitFuture<'a> {
+    type Output = Result<BlockingPermit<'a>, Canceled>;
+
+    #[inline]
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+        -> Poll<Self::Output>
+    {
+        if self.acquired.load(Ordering::SeqCst) {
+            // TODO: Or use a dedicated error for over `poll`ing?
+            return Poll::Ready(Err(Canceled))
+        }
+
+        let mut permit = Permit::new();
+        match permit.poll_acquire(cx, self.semaphore) {
+            Poll::Ready(Ok(())) => {
+                if !self.acquired.swap(true, Ordering::SeqCst) {
+                    eprintln!("Creating BlockingPermit (with permit)");
+                    Poll::Ready(Ok(BlockingPermit {
+                        permit: Some((permit, self.semaphore)),
+                        entered: AtomicBool::new(false)
+                    }))
+                } else {
+                    Poll::Ready(Err(Canceled))
+                }
+            }
+            Poll::Ready(Err(_)) => Poll::Ready(Err(Canceled)),
+            Poll::Pending => Poll::Pending,
         }
     }
 }
 
-pub enum NotPermitted {
-    /// The current thread is and must remain a reactor thread, where blocking
-    /// is disallowed. The current thread runtime is in use.
-    IsReactorThread,
+impl<'a> BlockingPermit<'a> {
+    /// Enter the blocking section of code on the current thread.  This is a
+    /// required secondary step from the [`BlockingPermitFuture`] and
+    /// [`blocking_permit`] call, because it _must_ be performed on the same
+    /// thread as the blocking section.  The blocking permit should then be
+    /// dropped at the end of the blocking section.
+    pub fn enter(&self) {
+        if !self.entered.swap(true, Ordering::SeqCst) {
+            // TODO: enter_blocking_section()
+        } else {
+            panic!("BlockingPermit::enter called twice!");
+            // TODO: Or just make this a log warning?
+        }
+    }
 
-    /// Dispatched task was canceled before it completed or awaited `Semaphore`
-    /// closed before being acquired.
-    Canceled,
+    // TODO: provide manual (ahead of drop) exit(), if also desired?
 }
+
+impl<'a> Drop for BlockingPermit<'a> {
+    fn drop(&mut self) {
+        if let Some((ref mut permit, ref semaphore)) = self.permit {
+            eprintln!("Dropping BlockingPermit, releasing semaphore");
+            permit.release(semaphore);
+        }
+        if self.entered.load(Ordering::SeqCst) {
+            // TODO: exit_blocking_section()
+            eprintln!("Dropped (entered) BlockingPermit");
+        } else {
+            eprintln!("Dropped (never entered) BlockingPermit!");
+        }
+    }
+}
+
+/// Error returned as output from [`BlockingPermitFuture`] if canceled, e.g. if
+/// the associated `Semaphore` is closed.
+#[derive(Debug)]
+pub struct Canceled;
+
+impl fmt::Display for Canceled {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Waiting for a blocking permit was canceled")
+    }
+}
+
+impl std::error::Error for Canceled {}
+
+/// Error returned by [`blocking_permit_future`] if the current thread is a
+/// fixed reactor thread, e.g. current thread runtime. This is recoverable by
+/// using [`dispatch_blocking`] instead.
+#[derive(Debug)]
+pub struct IsReactorThread;
+
+impl fmt::Display for IsReactorThread {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Can't block a fixed reactor thread, \
+                   e.g. current thread runtime, \
+                   must dispatch instead)")
+    }
+}
+
+impl std::error::Error for IsReactorThread {}
 
 /// Request a permit to perform a blocking operation on the current thread.
-/// Attempts to obtain a permit from the given Semaphore, and if returned,
-/// blocking operations are allowed while the `BlockingPermit` remains in
-/// scope. If no permits are available, the current task context will be
-/// notified when a permit becomes available.
-pub fn blocking_permit<'a>(semaphore: &'a Semaphore, cx: &mut Context<'_>)
-    -> Poll<Result<BlockingPermit<'a>, NotPermitted>>
+///
+/// The returned future attempts to obtain a permit from the provided
+/// `Semaphore` and outputs a `BlockingPermit` which can then be
+/// [`enter`](BlockingPermit::enter)ed to allow blocking or "long running"
+/// operation, while the `BlockingPermit` remains in scope. If no permits are
+/// immediately available, then the current task context will be notified when
+/// one becomes available.
+///
+/// If the number of blocking threads need not be constrained by a `Semaphore`,
+/// then this operation can be short circuited via [`blocking_permit`].
+///
+/// This returns an `IsReactorThread` error if the current thread can't become
+/// blocking, e.g. is a current thread runtime. This is recoverable by using
+/// [`dispatch_blocking`] instead.
+pub fn blocking_permit_future(semaphore: &Semaphore)
+    -> Result<BlockingPermitFuture<'_>, IsReactorThread>
 {
-    // TODO: Check if on current thread runtime?
-
-    let mut permit = Permit::new();
-    let ret = match permit.poll_acquire(cx, semaphore) {
-        Poll::Pending => Poll::Pending,
-        Poll::Ready(Ok(_)) => {
-            eprintln!("Creating BlockingPermit (live)");
-            Poll::Ready(Ok(BlockingPermit {
-                permit: Some((permit, semaphore))
-            }))
-        },
-        Poll::Ready(Err(_)) => Poll::Ready(Err(NotPermitted::Canceled)),
-    };
-
-    if let Poll::Ready(Ok(_)) = ret {
-        // TODO: enter_blocking_section()
+    // TODO: Really test if on the current thread runtime
+    if false {
+        return Err(IsReactorThread);
     }
-    ret
+
+    Ok(BlockingPermitFuture {
+        semaphore,
+        acquired: AtomicBool::new(false)
+    })
 }
 
-/// Request a permit to perform a blocking operation on the current thread,
-/// unlimited by any maximum number of threads.  May still return
-/// `NotPermitted::IsReactorThread`.
-pub fn blocking_permit_unlimited<'a>() -> Result<BlockingPermit<'a>, NotPermitted>
+/// Immediately return a permit which should then be
+/// [`enter`](BlockingPermit::enter)ed to allow a blocking or "long running"
+/// operation to be performed on the current thread.
+///
+/// This variant is unconstrained by any maximum allowed number of threads.
+/// To avoid and unbounded number of blocking threads being created, and
+/// possible resource exhaustion, use `blocking_permit_future` or
+/// `dispatch_blocking` instead.
+///
+/// This returns an `IsReactorThread` error if the current thread can't become
+/// blocking, e.g. is a current thread runtime. This is recoverable by using
+/// [`dispatch_blocking`] instead.
+pub fn blocking_permit<'a>() -> Result<BlockingPermit<'a>, IsReactorThread>
 {
-    // TODO: Check if on current thread runtime?
+    // TODO: Really test if on the current thread runtime
+    if false {
+        return Err(IsReactorThread);
+    }
 
-    // TODO: enter_blocking_section()
+    eprintln!("Creating BlockingPermit (unlimited)");
 
-    eprintln!("Creating BlockingPermit (dead)");
-
-    Ok(BlockingPermit { permit: None })
+    Ok(BlockingPermit {
+        permit: None,
+        entered: AtomicBool::new(false)
+    })
 }
 
-/// Use oneshot channel, and its error type, for our prototype custom Future
-pub type DispatchBlocking<T> = futures::channel::oneshot::Receiver<T>;
-pub type Canceled = futures::channel::oneshot::Canceled;
+// TODO: For now just using tokio_sync::oneshot channel, its error type, and
+// `Receiver` for our prototype custom `Future`. Should this be wrapped with a
+// new type?
+
+/// A future type created by [`dispatch_blocking`].
+pub type DispatchBlocking<T> = oneshot::Receiver<T>;
 
 /// Dispatch a blocking operation in the closure to a non-reactor thread, and
 /// return a future representing its return value.
@@ -117,24 +211,20 @@ pub fn dispatch_blocking<T>(f: Box<dyn FnOnce() -> T + Send>)
     rx
 }
 
-/*
-pub struct DispatchBlocking<T> {
-}
-
-impl<T> Future for DispatchBlocking<T> {
-    type Output = T;
-}
-*/
-
 #[cfg(test)]
 mod tests {
     use std::future::Future;
+    use std::panic::UnwindSafe;
     use std::pin::Pin;
+    use std::time::Duration;
 
-    use futures::task::noop_waker;
+    use futures::executor::block_on;
+    use futures::future::TryFutureExt;
 
     use super::*;
 
+    // TODO: Pretend for now that this is part of tokio-fs and also (somehow?)
+    // configurable.
     pub mod tokio_fs {
         use lazy_static::lazy_static;
         use tokio_sync::semaphore::Semaphore;
@@ -144,78 +234,123 @@ mod tests {
         }
     }
 
+    fn is_send<T: Send>() -> bool { true }
+
     #[allow(dead_code)]
-    struct TestFuture {
-        dispatched: Option<DispatchBlocking<usize>>
-    }
+    fn is_unwind_safe<T: UnwindSafe>() -> bool { true }
 
-    impl TestFuture {
-        #[allow(dead_code)]
-        fn new() -> Self {
-            TestFuture { dispatched: None }
-        }
-    }
+    #[test]
+    fn test_blocking_permit_traits() {
+        assert!(is_send::<BlockingPermit<'_>>());
 
-    impl Future for TestFuture {
-        type Output = Result<usize, Canceled>;
-
-        fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-            if let Some(ref mut blocking) = self.dispatched {
-                // FIXME: Unset self.dispatch on completion?
-                return Pin::new(&mut *blocking).poll(cx);
-            }
-            match blocking_permit(&tokio_fs::BLOCKING_SET, cx) {
-                Poll::Pending => return Poll::Pending,
-                Poll::Ready(Ok(_p)) => {
-                    eprintln!("do some blocking stuff (41)");
-                    Poll::Ready(Ok(41))
-                }
-                Poll::Ready(Err(NotPermitted::IsReactorThread)) => {
-                    self.dispatched = Some(
-                        dispatch_blocking( Box::new(|| -> usize {
-                            eprintln!("do some blocking stuff (42)");
-                            42
-                        }))
-                    );
-                    // FIXME: Unset self.dispatch on (early)completion?
-                    Pin::new(self.dispatched.as_mut().unwrap()).poll(cx)
-                }
-                Poll::Ready(Err(_)) => {
-                    Poll::Ready(Err(Canceled {}))
-                }
-            }
-        }
+        // TODO: its not UnwindSafe because `semaphore::Permit` is not.  Does
+        // it need to be?
+        // assert!(is_unwind_safe::<BlockingPermit<'_>>());
     }
 
     #[test]
     fn unlimited() {
-        match blocking_permit_unlimited() {
-            Ok(_p) => {
+        match blocking_permit() {
+            Ok(p) => {
+                p.enter();
                 eprintln!("do some blocking stuff");
             },
             Err(_e) => panic!("errored")
         }
     }
 
+    /// Test of a manually-constructed future which uses
+    /// `blocking_permit_future` and `dispatch_blocking`.
+    struct TestFuture<'a> {
+        delegate: Delegate<'a>,
+    }
+
+    enum Delegate<'a> {
+        Dispatch(DispatchBlocking<usize>),
+        Permit(BlockingPermitFuture<'a>),
+        None
+    }
+
+    impl<'a> TestFuture<'a> {
+        fn new() -> Self {
+            TestFuture { delegate: Delegate::None }
+        }
+    }
+
+    impl<'a> Future for TestFuture<'a> {
+        type Output = Result<usize, Canceled>;
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>)
+            -> Poll<Self::Output>
+        {
+            match self.delegate {
+                Delegate::None => {
+                    match blocking_permit_future(&tokio_fs::BLOCKING_SET) {
+                        Err(IsReactorThread) => {
+                            self.delegate = Delegate::Dispatch(
+                                dispatch_blocking( Box::new(|| -> usize {
+                                    eprintln!("do some blocking stuff (dispatched)");
+                                    thread::sleep(Duration::from_millis(100));
+                                    42
+                                }))
+                            );
+                        },
+                        Ok(f) => {
+                            self.delegate = Delegate::Permit(f);
+                        }
+                    }
+                    self.poll(cx) // recurse once, with delegate in place
+                                  // (needed for correct waking)
+                }
+                Delegate::Dispatch(ref mut db) => {
+                    eprintln!("delegate poll to DispatchBlocking");
+                    Pin::new(&mut *db).poll(cx).map_err( |_| Canceled)
+                }
+                Delegate::Permit(ref mut pf) => {
+                    eprintln!("delegate poll to BlockingPermitFuture");
+                    match Pin::new(&mut *pf).poll(cx) {
+                        Poll::Pending => Poll::Pending,
+                        Poll::Ready(Ok(p)) => {
+                            p.enter();
+                            eprintln!("do some blocking stuff (permitted)");
+                            Poll::Ready(Ok(41))
+                        }
+                        Poll::Ready(Err(_)) => Poll::Ready(Err(Canceled))
+                    }
+                }
+            }
+        }
+    }
+
     #[test]
-    fn limited() {
-        let semaphore = Semaphore::new(1);
-        let waker = noop_waker();
-        let mut cx = Context::from_waker(&waker);
-        let permit1 = blocking_permit(&semaphore, &mut cx);
-        match permit1 {
-            Poll::Pending => panic!("pending?"),
-            Poll::Ready(Ok(_)) => {
-                eprintln!("do first blocking stuff");
-            },
-            Poll::Ready(Err(_e)) => panic!("errored")
-        }
-        let permit2 = blocking_permit(&semaphore, &mut cx);
-        match permit2 {
-            Poll::Pending => eprintln!("permits exhausted"),
-            Poll::Ready(_) => panic!("permit2 ready?"),
-        }
-        drop(permit2);
-        drop(permit1);
+    fn future() {
+        let val = block_on(TestFuture::new()).expect("success");
+        assert!(val == 41 || val == 42);
+    }
+
+    #[test]
+    fn async_block_await() {
+        // Note how async/await makes this a lot nicer than the above
+        // `TestFuture` manual way.
+        let task = async {
+            match blocking_permit_future(&tokio_fs::BLOCKING_SET) {
+                Err(IsReactorThread) => {
+                    dispatch_blocking( Box::new(|| -> usize {
+                        eprintln!("do some blocking stuff (dispatched)");
+                        41
+                    }))
+                        .map_err( |_| Canceled)
+                        .await
+                }
+                Ok(f) => {
+                    let permit = f.await?;
+                    permit.enter();
+                    eprintln!("do some blocking stuff (permitted)");
+                    Ok(42)
+                }
+            }
+        };
+        let val = block_on(task).expect("task success");
+        assert!(val == 41 || val == 42);
     }
 }
