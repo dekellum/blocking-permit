b51355e4426b78e47d2e07f1c62fe37b9451ab36
David Kellum <dek-oss@gravitext.com>
2019-12-15 10:30:22 -0800

    cleanup sync asserts

--- src/tests.rs
+++ src/tests.rs
@@ -23,15 +23,13 @@ lazy_static! {
 }
 
 fn is_send<T: Send>() -> bool { true }
-
-#[allow(dead_code)] fn is_sync<T: Sync>() -> bool { true }
+fn is_sync<T: Sync>() -> bool { true }
 
 #[allow(dead_code)] fn is_unwind_safe<T: UnwindSafe>() -> bool { true }
 
 #[test]
 fn test_blocking_permit_traits() {
     assert!(is_send::<BlockingPermit<'_>>());
-    // TODO: its not: assert!(is_sync::<BlockingPermit<'_>>());
 
     // TODO: its not UnwindSafe because internals are not.  Does it need to be?
     // assert!(is_unwind_safe::<BlockingPermit<'_>>());
